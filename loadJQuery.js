61176cc49597e395b4913da29c50ed108de8abde
Dante Yan <ysh_008@hotmail.com>
2013-03-15 13:25:48 -0400

    Initial version
    contains js resizing window for mobile testing and load jquery in Chrome
    dev mode.

---
+++ loadJQuery.js
@@ -0,0 +1,4 @@
+var jq = document.createElement('script');
+jq.src = "http://code.jquery.com/jquery-latest.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
+jQuery.noConflict();
---
+++ resizeWindow.js
@@ -0,0 +1,1 @@
+var jsTestWindow = window.open(window.location); jsTestWindow.resizeTo(1024,768);
